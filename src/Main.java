94dc4fdd8f68fc6e18d355dc6ec404901ac3c91e
Ricardo Barreto de Araujo <rbbarreto@hotmail.com>
2021-12-27 23:33:10 -0300

    Classes de Mentoria e Cursos

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso de Java");
+        curso1.setDescricao("descricao curso de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso de Java");
+        curso2.setDescricao("descricao curso de JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("curso de Java");
+        mentoria.setDescricao("descricao curso de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+
+    }
+}
